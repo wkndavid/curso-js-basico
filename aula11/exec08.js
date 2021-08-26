d83e5c50efedb3cdf8cce0f9f11f3b76729a5ef4
wkndavid <workingnown@gmail.com>
2021-08-26 11:24:01 -0300

    Condiçoes part I: if e else

---
+++ aula11/exec08.js
@@ -0,0 +1,7 @@
+//console.log('O console funcionou corretamente !')
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel >= 60) { //Condição Simples!
+    console.log(`Você ultrapassou o limite de velocidade! Está Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/exec09.js
@@ -0,0 +1,6 @@
+var país = 'Brasil' 
+if (país == 'Brasil') {
+    console.log('Voce é Brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
