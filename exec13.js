431de1696772b6fa4f985a908bc5a418c0e44945
wkndavid <workingnown@gmail.com>
2021-08-26 22:17:46 -0300

    Condiçoes part II: Condições Compostas e Múltiplas

---
+++ exec13.js
@@ -0,0 +1,39 @@
+var agora = new Date()
+var diaSem = agora.getDay() 
+
+/*
+0 = Domingo
+1 = Segunda
+2 = Terça
+3 = Quarta
+4 = Quinta
+5 = Sexta
+6 = Sábado
+*/
+
+switch (diaSem) {
+    case 0:
+        console.log('Hoje é Domingo !')
+        break
+    case 1:
+        console.log('Hoje é Segunda-Feira !')
+        break  
+    case 2:
+        console.log('oje é Terça-Feira !')
+        break
+    case 3:
+        console.log('Hoje é Quarta-Feira !')
+        break
+    case 4:
+        console.log('Hoje é Quint-Feira !')
+        break
+    case 5:
+        console.log('Hoje é Sexta-Feira !')
+        break;
+    case 6:
+        console.log('Hoje é Sábado !')
+        break    
+    default:
+        console.log('ERRO: Digite um número válido!')
+        break
+}
