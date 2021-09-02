bc0e2be8e9482fa622ecedb8c1ab4b1c9bf20740
wkndavid <workingnown@gmail.com>
2021-09-01 21:38:33 -0300

    Exercicios de Repetiçoes - Vamos contar

--- aula14exec/script.js
+++ aula14exec/script.js
@@ -5,8 +5,27 @@ function contar() {
     let res = document.querySelector('div#res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+       // window.alert('[ERRO] Faltam dados!')
     } else {
-        alert('Tudo Ok!')
+        res.innerHTML = 'Contatando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p == 0) {
+            alert('Passo inválido! Consideradno Passo 1!')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            // Contagem Regressiva
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += ` \u{1f3c1}`
     }
-}.
+}
